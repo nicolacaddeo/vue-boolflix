b213c1ee26d258d5950caa551afe596d972d67f7
nicolacaddeo <nicola.caddeo58@gmail.com>
2022-10-04 12:38:28 +0200

    added input to search for movies

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,40 @@
 <template>
   <div id="app">
-
+    <header>
+      <h1>Boolfix</h1>
+      <InputComponent @search="searchFilm"/>
+    </header>
   </div>
 </template>
 
 <script>
   import {apiKey} from '@/data/env.js'
   import axios from 'axios'
+  import InputComponent from '@/components/InputComponent.vue'
+  
   
   export default {
   components: {
-    
-  },
+    InputComponent
+},
   data() {
     return {
       apiKey
     }
   },
   created() {
-    axios.get(`https://api.themoviedb.org/3/movie/550?api_key=${apiKey}`)
-    .then(({status, data})=>{
+    
+  },
+  methods: {
+    searchFilm(searchedFilm) {
+      axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${searchedFilm}&language=it-IT`)
+      .then(({status, data})=>{
       if (status === 200) {
         console.log(data);
         
       }
     })
+    }
   }
 }
 </script>
@@ -32,4 +42,18 @@
 <style lang="scss">
 @import './style/main_style.scss';
 
+header {
+  height: 10vh;
+  padding: 1.5rem;
+  background-color: #111;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  h1 {
+    color: #990000;
+    text-transform: uppercase;
+  }
+}
 </style>
+  
+    
---
+++ src/components/InputComponent.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <input type="text" v-model="searchedFilm">
+    <button @click="$emit('search', searchedFilm)">Search</button>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'InputComponent',
+    data(){
+        return{
+            searchedFilm: ''
+        }
+    }
+}
+</script>
+
+<style scoped lang="scss">
+    input {
+        margin-right: 1rem;
+        padding: .2rem .5rem;
+    }
+    button {
+        padding: .2rem .5rem;
+    }
+</style>
